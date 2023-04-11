7870c709f86f7ddccd893fe143a0ad4d2a2fad3b
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-11 15:06:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,5 +4,5 @@ import numpy as np
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    _, p_value = stats.ks_2samp(x, y)
+    t_stat, p_value = stats.ttest_ind(x, y)
     return p_value < 0.01
