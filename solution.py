00be41c8ae302d0f972b94f32308844e65d61383
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-11 16:49:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,5 +4,5 @@ import numpy as np
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    w_stat, p_value = stats.wilcoxon(x, y)
+    _, p_value = stats.anderson_ksamp(x, y)
     return p_value < 0.01
